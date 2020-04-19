9b00969362dd44a0740a50f978f8a7a9f128112e
Roger-Maciel <63749589+Roger-Maciel@users.noreply.github.com>
2020-04-19 18:27:29 -0300

    Update ExemploGradleRepositoryClienteComLombokApplication.java

--- ExemploGradleRepositoryClienteComLombok/src/main/java/maciel/roger/ExemploGradleRepositoryClienteComLombok/ExemploGradleRepositoryClienteComLombokApplication.java
+++ ExemploGradleRepositoryClienteComLombok/src/main/java/maciel/roger/ExemploGradleRepositoryClienteComLombok/ExemploGradleRepositoryClienteComLombokApplication.java
@@ -3,6 +3,11 @@ package maciel.roger.ExemploGradleRepositoryClienteComLombok;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+    @author Roger Maciel
+    @since 16/04/2020
+    @version 1.0
+ */
 @SpringBootApplication
 public class ExemploGradleRepositoryClienteComLombokApplication {
 
